bcd7f60901f60362529ffcc1bac99732b75d5834
sganzerla <alisson_sganzerla@yahoo.com.br>
2020-02-06 23:49:52 -0300

    incrementando sequencia de luzes

--- arduino-genius/genius.ino
+++ arduino-genius/genius.ino
@@ -1,21 +1,74 @@
-// porta onde está conectado o led
-int ledBranco = 2;
+
+#define LED_VERDE 2
+#define LED_AMARELO 3
+#define LED_VERMELHO 4
+#define LED_AZUL 5
+
+#define UM_SEGUNDO 1000
+#define MEIO_SEGUNDO 500
+
+#define TAMANHO_SEQUENCIA 4
+
+int sequenciaLuzes[TAMANHO_SEQUENCIA];
 
 void setup()
 {
-    // pino 2
-    pinMode(ledBranco, OUTPUT);
+    Serial.begin(9600);
+    iniciaPortas();
+    iniciaJogo();
+}
+
+void iniciaJogo()
+{
+    sequenciaLuzes[0] = LED_AZUL;
+    sequenciaLuzes[1] = LED_VERDE;
+    sequenciaLuzes[2] = LED_VERMELHO;
+    sequenciaLuzes[3] = LED_AMARELO;
+}
+
+void iniciaPortas()
+{
+    pinMode(LED_VERDE, OUTPUT);
+    pinMode(LED_AMARELO, OUTPUT);
+    pinMode(LED_VERMELHO, OUTPUT);
+    pinMode(LED_AZUL, OUTPUT);
 }
 
 void loop()
 {
-    // liga luz
-    digitalWrite(ledBranco, HIGH);
+    for (int indice = 0; indice < TAMANHO_SEQUENCIA; indice++)
+    {
+        piscaLed(sequenciaLuzes[indice]);
+    }
+}
 
-    delay(1000);
+void piscaSequencia1()
+{
+    piscaLed(LED_VERMELHO);
+    piscaLed(LED_AMARELO);
+    piscaLed(LED_VERDE);
+    piscaLed(LED_AZUL);
+    delay(MEIO_SEGUNDO);
+}
 
-    // desliga luz
-    digitalWrite(ledBranco, LOW);
+void piscaLedSequencia2()
+{
+    digitalWrite(LED_VERDE, HIGH);
+    digitalWrite(LED_AMARELO, HIGH);
+    digitalWrite(LED_VERMELHO, HIGH);
+    digitalWrite(LED_AZUL, HIGH);
+    delay(UM_SEGUNDO);
+    digitalWrite(LED_VERDE, LOW);
+    digitalWrite(LED_AMARELO, LOW);
+    digitalWrite(LED_VERMELHO, LOW);
+    digitalWrite(LED_AZUL, LOW);
+    delay(MEIO_SEGUNDO);
+}
 
-    delay(1000);
+piscaLed(int portaLed)
+{
+    digitalWrite(portaLed, HIGH);
+    delay(UM_SEGUNDO);
+    digitalWrite(portaLed, LOW);
+    delay(MEIO_SEGUNDO);
 }
