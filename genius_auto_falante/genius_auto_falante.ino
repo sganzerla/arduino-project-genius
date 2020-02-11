da50d781f226c2eeaa8e822c8df6799442d0d2eb
sganzerla <alisson_sganzerla@yahoo.com.br>
2020-02-11 01:03:00 -0300

    corrigir leds piscando intermitente

--- genius_auto_falante/genius_auto_falante.ino
+++ genius_auto_falante/genius_auto_falante.ino
@@ -1,17 +1,13 @@
-// DECLARAÇÃO DE CONSTANTES
-int RODADA = 0;
-int PASSO = 0;
-bool GAME_OVER = false;
-
-// DECLARAÇÃO  DE ARRAYS
-int SEQUENCIA[32] = {};
-int BOTOES[4] = {8, 9, 10, 11};
-int LEDS[4] = {2, 3, 4, 5};
-int NOTAS_MUSICAIS[4] = {262, 294, 330, 349};
-
+int sequencia[32] = {};
+int botoes[4] = {8, 9, 10, 11};
+int leds[4] = {2, 3, 4, 5};
+int notas_musicais[4] = {262, 294, 330, 349};
+int rodada = 0;
+int passo = 0;
+bool game_over = false;
 
 void setup() {
-  // LEDS
+  // 
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
@@ -23,77 +19,77 @@ void setup() {
   pinMode(9, INPUT_PULLUP);
   pinMode(10, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP);
-  randomSeed(analogRead(A0));
+  randomSeed(analogRead(0));
 
   Serial.begin(9600);
 
 }
 
 void loop() {
-  proximaRODADA();
-  reproduzirSequencia();
+  proximaRodada();
+  reproduzirsequencia();
   aguardarJogador();
   // estrutura usada para reiniciar todas as variáveis (novo jogo)
-  if (GAME_OVER == true) {
-    SEQUENCIA[32] = {};
-    RODADA = 0;
-    PASSO = 0;
-    GAME_OVER = false;
+  if (game_over == true) {
+    sequencia[32] = {};
+    rodada = 0;
+    passo = 0;
+    game_over = false;
   }
   delay(1000);
 }
 
 /************************* Funções **************************/
 
-void proximaRODADA() {
+void proximaRodada() {
   int sorteio = random(4);
-  SEQUENCIA[RODADA] = sorteio;
-  RODADA = RODADA + 1;
-  Serial.println("Sorteio: "+ sorteio);
+  sequencia[rodada] = sorteio;
+  rodada = rodada + 1;
+  Serial.println( sequencia[rodada]);
 }
 
-void reproduzirSequencia() {
-  for (int i = 0; i < RODADA; i++) {
-   //  tone(7, NOTAS_MUSICAIS[SEQUENCIA[i]]);
-    digitalWrite(LEDS[SEQUENCIA[i]], HIGH);
+void reproduzirsequencia() {
+  for (int i = 0; i < rodada; i++) {
+   //  tone(7, notas_musicais[sequencia[i]]);
+    digitalWrite(leds[sequencia[i]], HIGH);
     delay(500);
    //  noTone(7);
-    digitalWrite(LEDS[SEQUENCIA[i]], LOW);
+    digitalWrite(leds[sequencia[i]], LOW);
     delay(500); // 100
   }
 }
 
 void aguardarJogador() {
   int botao_pressionado = 0;
-  for (int i = 0; i < RODADA; i++) {
+  for (int i = 0; i < rodada; i++) {
     bool jogada_efetuada = false;
     while (!jogada_efetuada) {
       for (int i = 0; i <= 3; i++) {
-        if (digitalRead(BOTOES[i]) == HIGH) {
+        if (digitalRead(botoes[i]) == HIGH) {
           botao_pressionado = i;
-        //  tone(7, NOTAS_MUSICAIS[i]);
-          digitalWrite(LEDS[i], HIGH);
+        //  tone(7, notas_musicais[i]);
+          digitalWrite(leds[i], HIGH);
           delay(300);
-          digitalWrite(LEDS[i], LOW);
+          digitalWrite(leds[i], LOW);
          // noTone(7);
           jogada_efetuada = true;
         }
       }
     }
     //verificar a jogada
-    if (SEQUENCIA[PASSO] != botao_pressionado) {
+    if (sequencia[passo] != botao_pressionado) {
       //efeito luminoso/sonoro indicando que o jogador perdeu
       for (int i = 0; i <= 3; i++) {
        // tone (7, 70);
-        digitalWrite(LEDS[i], HIGH);
+        digitalWrite(leds[i], HIGH);
         delay(500); // 100
-        digitalWrite(LEDS[i], LOW);
+        digitalWrite(leds[i], LOW);
     //  noTone(7);
       }
-      GAME_OVER = true;
+      game_over = true;
       break;
     }
-    PASSO = PASSO + 1;
+    passo = passo + 1;
   }
-  PASSO = 0;
+  passo = 0;
 }
