53f10ac1ec3052c98aa103e57c41fbaf4e3fe3af
sganzerla <alisson_sganzerla@yahoo.com.br>
2020-02-15 00:18:54 -0300

    implementando interação com jogador e pegando input dos botoes - incompleto

--- tinker_kad/tinker_kad.ino
+++ tinker_kad/tinker_kad.ino
@@ -11,30 +11,82 @@
 #define TIMER_500
 #define TIMER_1000
 
-
-int array_leds[] = { LED_BRANCO, LED_AMARELO, LED_LARANJA, LED_AZUL };
+int array_buttons[] = {B_BRANCO, B_AMARELO, B_LARANJA, B_AZUL};
+int array_leds[] = {LED_BRANCO, LED_AMARELO, LED_LARANJA, LED_AZUL};
 int array_leds_sort[100] = {};
-int rand_number;
-int rounds = 0; 
-
+int rounds = 0;
+int step = 0;
+bool game_over = false;
+bool action_player = false;
 
 void setup()
 {
-    Serial.begin(9600);
-	startPort();
-  	randomSeed(analogRead(0));
+  Serial.begin(9600);
+  startPort();
+  randomSeed(analogRead(0));
 }
 
 void loop()
-{ 
-    newRound();
-	playRound();   
-  
+{
+  newRound();
+  playRound();
+  //  waitPressPlayer();
+  //  theEndGame();
 }
 
-void playRound(){
+void theEndGame()
+{
+  if (game_over)
+  {
+    array_leds_sort[100] = {};
+    rounds = 0;
+    step = 0;
+    game_over = false;
+  }
+}
+
+int watch_action_player()
+{
+  for (int i = 0; i <= 3; i++)
+  {
+    if (digitalRead(array_buttons[i]) == HIGH)
+    {
+      setOnLed(array_leds[array_leds_sort[i]]);
+      action_player = true;
+      return i;
+    }
+  }
+}
+
+void waitPressPlayer()
+{
+  int button_pressed = 0;
+  for (int i = 0; i < rounds; i++)
+  {
+
+    while (!action_player)
+    {
+      button_pressed = watch_action_player();
+    }
+
+    if (array_leds[array_leds_sort[step]] != button_pressed)
+    {
+      for (int i = 0; i <= 3; i++)
+      {
+        setOnLed(array_leds[array_leds_sort[i]]);
+      }
+      game_over = true;
+      break;
+    }
+    step++;
+  }
+  step = 0;
+}
+
+void playRound()
+{
   Serial.println();
-  Serial.print("Round: ");  
+  Serial.print("Round: ");
   Serial.println(rounds);
 
   for (int i = 0; i < rounds; i++)
@@ -43,24 +95,22 @@ void playRound(){
     Serial.print(array_leds[array_leds_sort[i]]);
     Serial.print(" ");
     setOnLed(array_leds[array_leds_sort[i]]);
-  } 
+  }
   delay(2000);
 }
 
 void newRound()
-{  
-  	rounds = rounds + 1;
-  	rand_number = random(4);
-	array_leds_sort[rounds] = rand_number;
-    Serial.println();
+{
+  rounds++;
+  array_leds_sort[rounds] = random(4);
 }
 
 void setOnLed(int port)
 {
-   digitalWrite(port, HIGH);
-   delay(1000); 
-   digitalWrite(port, LOW);
-   delay(500);
+  digitalWrite(port, HIGH);
+  delay(1000);
+  digitalWrite(port, LOW);
+  delay(500);
 }
 
 void startPort()
@@ -69,9 +119,9 @@ void startPort()
   pinMode(LED_AMARELO, OUTPUT);
   pinMode(LED_LARANJA, OUTPUT);
   pinMode(LED_AZUL, OUTPUT);
-  
+
   pinMode(B_BRANCO, INPUT_PULLUP);
   pinMode(B_AMARELO, INPUT_PULLUP);
   pinMode(B_LARANJA, INPUT_PULLUP);
-  pinMode(B_AZUL, INPUT_PULLUP); 
+  pinMode(B_AZUL, INPUT_PULLUP);
 }
