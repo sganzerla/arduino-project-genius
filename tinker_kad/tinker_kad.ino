99e7893e6e23cbd41aa724fa8eb8a08199387cad
sganzerla <alisson_sganzerla@yahoo.com.br>
2020-02-14 22:27:49 -0300

    sorteando cores e armazenando  rodadas

--- tinker_kad/tinker_kad.ino
+++ tinker_kad/tinker_kad.ino
@@ -11,19 +11,48 @@
 #define TIMER_500
 #define TIMER_1000
 
-int ARRAY_LEDS[] = {LED_BRANCO, LED_AMARELO, LED_LARANJA, LED_AZUL};
+
+int array_leds[] = { LED_BRANCO, LED_AMARELO, LED_LARANJA, LED_AZUL };
+int array_leds_sort[100] = {};
+int rand_number;
+int rounds = 0; 
+
 
 void setup()
 {
-  startPort();
+    Serial.begin(9600);
+	startPort();
+  	randomSeed(analogRead(0));
 }
 
 void loop()
-{
-  for (int i = 0; i < 4; i++)
+{ 
+    newRound();
+	playRound();   
+  
+}
+
+void playRound(){
+  Serial.println();
+  Serial.print("Round: ");  
+  Serial.println(rounds);
+
+  for (int i = 0; i < rounds; i++)
   {
-    setOnLed(ARRAY_LEDS[i]);
-  }  
+    Serial.print(" ");
+    Serial.print(array_leds[array_leds_sort[i]]);
+    Serial.print(" ");
+    setOnLed(array_leds[array_leds_sort[i]]);
+  } 
+  delay(2000);
+}
+
+void newRound()
+{  
+  	rounds = rounds + 1;
+  	rand_number = random(4);
+	array_leds_sort[rounds] = rand_number;
+    Serial.println();
 }
 
 void setOnLed(int port)
